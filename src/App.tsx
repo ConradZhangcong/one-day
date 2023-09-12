cb09a2e977ec0318bbe6499033822177e86c2425
ConradZhangcong <heisenberg0519@outlook.com>
2023-09-12 19:42:34 +0800

    feat:add routers and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { RouterProvider } from "react-router-dom";
+
+import router from "./routers";
+
 function App() {
-  return <div>APP</div>;
+  return (
+    <div className="od-page">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/error/not-found.tsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div>
+      <div>页面找不到啦</div>
+    </div>
+  );
+};
+
+export default NotFound;
