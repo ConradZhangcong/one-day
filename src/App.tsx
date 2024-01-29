d4e383c720a15f604bbb07884b1a5d52069ceeaf
ConradZhangcong <heisenberg0519@outlook.com>
2024-01-29 19:35:49 +0800

    feat: add components and page

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <div></div>
       <h1>Vite + React</h1>
       <h1 className="tw-text-3xl tw-font-bold tw-underline">Hello world!</h1>
       <div className="card">
